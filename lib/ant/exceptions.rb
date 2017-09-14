45a63319341548f0dcd4a9fc2b4a80e381dcbbef
Gilberto Vargas <tachoguitar@gmail.com>
2017-09-13 23:52:31 -0500

    Implement the basic exceptions module

---
+++ lib/ant/exceptions.rb
@@ -0,0 +1,44 @@
+module Ant
+  module Exceptions
+    ##
+    # Exception used as standard error on this gem.
+    # @param message.  This param is meant to be read by another developer
+    #                  It would be nice if the message were self descriptive
+    #                  enough so the developer won't need to ask the creator
+    #                  of the app for help.
+    # @param code.     The code is similar to message, but for this case
+    #                  it is meant to be used for the program to handle
+    #                  exceptions and to make easier to identify the exception..
+    #                  The default is the exception class, so it won't change
+    #                  almost never.
+    # @param data.     Contains additional data to detail the error.
+    class AntBaseException < StandardError
+      attr_reader :message, :code, :data
+      def initialize(message, code, data)
+        @message = message
+        @code = code
+        @data = data
+      end
+    end
+
+    ##
+    # Is used to express a problem with the client's request.
+    class AntFail < AntBaseException
+      def initialize(message, code = nil, data = {})
+        code ||= self.class.name.split('::').last
+        super(message, code, data)
+      end
+    end
+
+    ##
+    # Is used to express an error that was found during the execution of the
+    # program but it also means that the invoked endpoint has not the power to
+    # fix it, so it will only complain.
+    class AntError < AntBaseException
+      def initialize(message, code = nil, data = {})
+        code ||= self.class.name.split('::').last
+        super(message, code, data)
+      end
+    end
+  end
+end
