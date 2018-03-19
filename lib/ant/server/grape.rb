3eb7c546029b42439e184f647b0ead478a105a08
Gilberto Vargas <tachoguitar@gmail.com>
2018-03-19 17:25:31 -0600

    Add a decorator for using ants with grape

---
+++ lib/ant/server/grape.rb
@@ -0,0 +1,48 @@
+module Ant
+  module Server
+    module GrapeDecorator
+      def self.handler
+        lambda do |env, level, ex|
+          params = env['api.endpoint'].params
+          request = env['api.endpoint'].request
+          pkg = RequestResponse.new(request: request, params: params)
+          pkg.exception = ex
+          Server::Response.logger.send(level, pkg)
+          Server::Response.format.send(level, pkg)
+        end
+      end
+
+      def self.configure_handlers(base)
+        Server::Response.resources(:exceptions).each do |exception_class, level|
+          base.rescue_from(exception_class) do |ex|
+            response = Ant::Server::GrapeDecorator.handler.call(env, level, ex)
+            error!(response, 400)
+          end
+        end
+        base.rescue_from(:all) do |ex|
+          response = Ant::Server::GrapeDecorator.handler.call(env, :fatal, ex)
+          error!(response, 400)
+        end
+      end
+
+      def self.included(base)
+        base.formatter(:json, lambda do |response, _|
+          pkg = RequestResponse.new(request: {}, params: {})
+          pkg.result = response
+          Server::Response.format.send(:success, pkg).to_json
+        end)
+        configure_logger(base)
+        configure_handlers(base)
+      end
+
+      def self.configure_logger(base)
+        base.after do
+          params = env['api.endpoint'].params
+          request = env['api.endpoint'].request
+          pkg = RequestResponse.new(request: request, params: params)
+          Server::Response.logger.access(pkg)
+        end
+      end
+    end
+  end
+end
