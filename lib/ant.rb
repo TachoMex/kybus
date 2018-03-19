611300703671bd09bc15008b62e91f166430a8c6
Gilberto Vargas <tachoguitar@gmail.com>
2018-03-18 23:52:15 -0600

    Fix broken references on requires

--- lib/ant.rb
+++ lib/ant.rb
@@ -1,4 +1,4 @@
 require_relative 'ant/version'
 require_relative 'ant/exceptions'
-require_relative 'ant/response'
-require_relative 'ant/format'
+require_relative 'ant/server/response'
+require_relative 'ant/server/format'
