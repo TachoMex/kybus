d89ef80e8c6e0fbbbb389fda6c980243d29b754d
Gilberto Vargas <tachoguitar@gmail.com>
2018-05-04 20:51:27 -0500

    Introduce nanoservice

---
+++ lib/ant/nanoservice.rb
@@ -0,0 +1,5 @@
+require_relative 'server/nanoservice/datasource/model'
+require_relative 'server/nanoservice/factory'
+require_relative 'server/nanoservice/datasource/json_repository'
+require_relative 'server/nanoservice/datasource/sequel'
+require_relative 'server/nanoservice/datasource/exceptions'
