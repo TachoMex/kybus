5dd0446fad9ee45d646bda45dffc8641a0d59e66
Gilberto Vargas <tachoguitar@gmail.com>
2018-05-04 20:46:49 -0500

    Add model class to wrap an actual object with its repository

---
+++ lib/ant/server/nanoservice/datasource/model.rb
@@ -0,0 +1,32 @@
+require_relative 'id_generators'
+module Ant
+  module Server
+    module Nanoservice
+      module Datasource
+        class Model
+          attr_reader :data
+          attr_writer :data
+
+          def run_validations!
+            puts 'WARN: model with no validations'
+          end
+
+          def store
+            run_validations!
+            @repository.store(@data)
+          end
+
+          def create
+            run_validations!
+            @repository.create(@data)
+          end
+
+          def initialize(data, repository = nil)
+            @data = data
+            @repository = repository || default_repository
+          end
+        end
+      end
+    end
+  end
+end
