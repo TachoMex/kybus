35d076bda7a4afe63746a04ad03175fdce3dfc6a
Ramon Lozano <ramon.lozano@kueski.com>
2018-07-14 00:21:37 -0500

    Add success exception example

--- routes/basic_auth_routes.rb
+++ routes/basic_auth_routes.rb
@@ -9,6 +9,16 @@ class BasicAuthRoutes < Grape::API
     end
   end
 
+  class AuthenticationSuccess < Ant::Exceptions::AntSuccess
+    def initialize(message, user)
+      super(message, nil, user)
+    end
+
+    def http_code
+      202
+    end
+  end
+
   module AuthHelper
     def current_user!
       token = env['HTTP_AUTHORIZATION']
@@ -16,7 +26,7 @@ class BasicAuthRoutes < Grape::API
       data = token.tr('Basic ', '')
       user, pass = Base64.decode64(data).split(':')
       raise(AuthenticationError) unless user == 'test' && pass == 'secret'
-      user
+      raise(AuthenticationSuccess.new('Authorized User', user))
     end
   end
 
