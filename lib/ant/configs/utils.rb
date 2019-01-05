df5f9df71f65525a1b53de82b9719456214aa78b
Gilberto Vargas <tachoguitar@gmail.com>
2019-01-05 17:12:24 -0600

    Implement helpers for loading env vars

--- lib/ant/configs/utils.rb
+++ lib/ant/configs/utils.rb
@@ -22,8 +22,20 @@ module Ant
         result
       end
 
+      def recursive_set(hash, key, value)
+        current = key[0]
+        if key.size == 1
+          hash[current] = value
+          hash
+        else
+          hash[current] ||= {}
+          recursive_set(hash[current], key[1..-1], value)
+        end
+      end
+
       def split_env_string(string)
-        string.split(/(?<!\\),/).map { |str| str.gsub('\,', ',') }
+        strings = string.split(/(?<!\\),/).map { |str| str.gsub('\,', ',') }
+        strings.size == 1 ? strings.first : strings
       end
     end
   end
