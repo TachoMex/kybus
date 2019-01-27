d58bae08b8bd570fbc7a304b74a18276dd57151a
Gilberto Vargas <tachoguitar@gmail.com>
2019-01-27 17:20:23 -0600

    Decouple env loader from env so it is easier to test

--- lib/ant/configs/loaders/env.rb
+++ lib/ant/configs/loaders/env.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 module Ant
   module Configuration
     module Loaders
@@ -9,21 +11,22 @@ module Ant
       #   export CONFIG_ENV_OBJ__VALUE=3 => { "env_obj" => { 'value' => '3' } }
       class Env
         include Ant::Configuration::Utils
-        def initialize(env_prefix, manager)
+        def initialize(env_prefix, manager, conf = ENV.to_h)
           @env_prefix = env_prefix
           @manager = manager
+          @env = conf
         end
 
         # Parses the configurations from ENV
         def load!
-          ENV.select { |str| str.start_with?(@env_prefix) }
-             .each_with_object({}) do |(k, v), h|
-               # Remove ENV prefix and conver to downcase, then split by '__'
-               clean_key = k.sub(/^#{@env_prefix}_?/, '').downcase.split('__')
-               # recursively create the objects to set the config where it
-               # should be
-               recursive_set(h, clean_key, split_env_string(v))
-             end
+          @env.select { |str, _| str.start_with?(@env_prefix) }
+              .each_with_object({}) do |(k, v), h|
+            # Remove ENV prefix and convert to downcase, then split by '__'
+            clean_key = k.sub(/^#{@env_prefix}_?/, '').downcase.split('__')
+            # recursively create the objects to set the config where it
+            # should be
+            recursive_set(h, clean_key, split_env_string(v))
+          end
         end
       end
     end
