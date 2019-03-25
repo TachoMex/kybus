1f27af2027b2dddd9b77c55e97659bb8f566f091
Gilberto Vargas <tachoguitar@gmail.com>
2019-03-25 00:45:57 -0600

    Add todo markers on daemon

--- lib/ant/dry/daemon.rb
+++ lib/ant/dry/daemon.rb
@@ -3,6 +3,11 @@
 module Ant
   module DRY
     # Use this for running tasks that will be looping by only sending a lambda
+    # TODO: Improve this class.
+    # - Add a maechanism for killing the process
+    # - Add a function to test how many threads are alive
+    # - Make it testable
+    # - Add unit tests
     class Daemon
       def initialize(wait_time, attach, retry_on_failure = false)
         @proc = -> { yield }
@@ -20,7 +25,9 @@ module Ant
           rescue StandardError => ex
             raise unless @retry_on_failure
 
+            # :nocov: #
             log_error('Unexpected error', error: ex)
+            # :nocov: #
           end
           sleep(@wait_time)
         end
@@ -30,13 +37,17 @@ module Ant
         if @attach
           task
         else
+          # :nocov: #
           @thread = Thread.new { task }
+          # :nocov: #
         end
       end
 
+      # :nocov: #
       def await
         @thread&.join
       end
+      # :nocov: #
     end
   end
 end
