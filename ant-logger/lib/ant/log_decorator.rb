2a00bc188c74649ef3b2a545790a9e4ff5ed785d
Gilberto Vargas <tachoguitar@gmail.com>
2019-05-06 01:42:10 -0500

    Implement Ant logger gem

---
+++ ant-logger/lib/ant/log_decorator.rb
@@ -0,0 +1,13 @@
+require 'ant/core'
+require 'json'
+
+require_relative 'logger/config'
+require_relative 'logger/format'
+require_relative 'logger/log_methods'
+
+
+module Ant
+  module Logger
+    include LogMethods
+  end
+end
