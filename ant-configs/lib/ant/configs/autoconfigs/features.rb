5499f050b1967fd1a470f4a4162d3ebe6632411e
Gilberto Vargas <tachoguitar@gmail.com>
2019-10-06 23:27:42 -0500

    Include feature flags in autoconfigs

---
+++ ant-configs/lib/ant/configs/autoconfigs/features.rb
@@ -0,0 +1,24 @@
+module Ant
+  module Configuration
+    module Autoconfigs
+      class Features
+        include Ant::Configuration::Utils
+
+        def self.from_config(config)
+          new(config)
+        end
+
+        def initialize(config)
+          @config = config
+          @connection = Ant::Configuration::FeatureFlag.new(symbolize(config))
+        end
+
+        def [](key)
+          @connection[key]
+        end
+      end
+
+      register('features', Features)
+    end
+  end
+end
