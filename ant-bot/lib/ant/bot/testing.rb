e1ce0e079a9650ff7126b5977d8e973632e20a7e
Gilberto Vargas <tachoguitar@gmail.com>
2020-04-12 18:16:45 -0500

    Add functions to enhance testing of bots

---
+++ ant-bot/lib/ant/bot/testing.rb
@@ -0,0 +1,26 @@
+require_relative 'adapters/debug'
+require_relative 'base'
+module Ant
+  module Bot
+
+    class NonDebugAdapterInTesting < StandardError
+    end
+
+    # Base class for bot implementation. It wraps the threads execution, the
+    # provider and the state storage inside an object.
+    class Base
+      include Ant::Bot::Adapter
+      def stub_channels(messages)
+        raise(NonDebugAdapterInTesting) unless @provider.is_a?(Debug)
+
+        @provider = Debug.new('channels' => messages)
+      end
+
+      def run_test
+        run
+      rescue Debug::NoMoreMessageException
+        true
+      end
+    end
+  end
+end
